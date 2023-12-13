65ff44c25dbfc6a304da0101107c4e98265fad6e
defaulter7045 <manish0076kumar@gmail.com>
2023-12-13 17:32:28 +0530

    Initial commit

---
+++ src/main/java/in/defaulter/movies/MovieRepository.java
@@ -0,0 +1,13 @@
+package in.defaulter.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    public Optional<Movie>findMovieByImdbId(String imdbdId);
+
+}
---
+++ src/main/java/in/defaulter/movies/ReviewRepository.java
@@ -0,0 +1,12 @@
+package in.defaulter.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+    public Optional<Movie> findReviewByImdbId(String imdbdId);
+}
